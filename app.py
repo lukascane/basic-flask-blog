08d6caecaff3606bb99459245b45df2198d8aede
LukasTrzcinski <lukastrz6@gmail.com>
2025-05-13 16:49:20 +0200

    redesign json file for likes and index.html

--- app.py
+++ app.py
@@ -3,46 +3,61 @@ import json
 import os
 
 app = Flask(__name__)
-
 DATA_FILE = 'posts.json'
+
 
 def load_posts():
     if not os.path.exists(DATA_FILE):
         return []
-    with open(DATA_FILE, 'r') as f:
-        return json.load(f)
+    with open(DATA_FILE, 'r') as file:
+        return json.load(file)
+
 
 def save_posts(posts):
-    with open(DATA_FILE, 'w') as f:
-        json.dump(posts, f, indent=4)
+    with open(DATA_FILE, 'w') as file:
+        json.dump(posts, file, indent=4)
 
-def get_post_by_id(post_id):
+
+def get_next_id(posts):
+    if not posts:
+        return 1
+    return max(post['id'] for post in posts) + 1
+
+
+def fetch_post_by_id(post_id):
     posts = load_posts()
     for post in posts:
         if post['id'] == post_id:
             return post
     return None
 
+
 @app.route('/')
 def index():
     posts = load_posts()
     return render_template('index.html', posts=posts)
 
+
 @app.route('/add', methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
+        author = request.form.get('author')
+        title = request.form.get('title')
+        content = request.form.get('content')
         posts = load_posts()
-        new_id = max([post['id'] for post in posts], default=0) + 1
         new_post = {
-            'id': new_id,
-            'author': request.form['author'],
-            'title': request.form['title'],
-            'content': request.form['content']
+            "id": get_next_id(posts),
+            "author": author,
+            "title": title,
+            "content": content,
+            "likes": 0
         }
         posts.append(new_post)
         save_posts(posts)
         return redirect(url_for('index'))
+
     return render_template('add.html')
+
 
 @app.route('/delete/<int:post_id>')
 def delete(post_id):
@@ -51,19 +66,38 @@ def delete(post_id):
     save_posts(posts)
     return redirect(url_for('index'))
 
+
 @app.route('/update/<int:post_id>', methods=['GET', 'POST'])
 def update(post_id):
     posts = load_posts()
-    post = get_post_by_id(post_id)
+    post = fetch_post_by_id(post_id)
     if not post:
         return "Post not found", 404
+
     if request.method == 'POST':
-        post['author'] = request.form['author']
-        post['title'] = request.form['title']
-        post['content'] = request.form['content']
+        post['author'] = request.form.get('author')
+        post['title'] = request.form.get('title')
+        post['content'] = request.form.get('content')
+        for i in range(len(posts)):
+            if posts[i]['id'] == post_id:
+                posts[i] = post
+                break
         save_posts(posts)
         return redirect(url_for('index'))
+
     return render_template('update.html', post=post)
+
+
+@app.route('/like/<int:post_id>')
+def like(post_id):
+    posts = load_posts()
+    for post in posts:
+        if post['id'] == post_id:
+            post['likes'] = post.get('likes', 0) + 1
+            break
+    save_posts(posts)
+    return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
