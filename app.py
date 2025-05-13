7d50ac261cfa0706f30bb54ead043e05ba4b1bff
LukasTrzcinski <lukastrz6@gmail.com>
2025-05-13 14:32:43 +0200

    added template and json and Fixed a bug in the login function and updated the styling

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
+def load_posts():
+    with open('posts.json', 'r') as f:
+        return json.load(f)
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    posts = load_posts()
+    return render_template('index.html', posts=posts)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
