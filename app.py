de75f0a3baaa5303c16c7ab40d750ea01b824ae5
LukasTrzcinski <lukastrz6@gmail.com>
2025-05-13 16:29:44 +0200

    Implement update blog post functionality, add update.html, app.py handle the updating of blog posts:T

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import json
 
 app = Flask(__name__)
 
+
 # Load posts from posts.json
 def load_posts():
     try:
@@ -11,15 +12,18 @@ def load_posts():
     except FileNotFoundError:
         return []  # Return an empty list if the file doesn't exist
 
+
 # Save posts to posts.json
 def save_posts(posts):
     with open('posts.json', 'w') as f:
         json.dump(posts, f, indent=4)
 
+
 @app.route('/')
 def index():
     posts = load_posts()
     return render_template('index.html', posts=posts)
+
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
@@ -45,6 +49,7 @@ def add():
         return redirect(url_for('index'))
     return render_template('add.html')
 
+
 @app.route('/delete/<int:post_id>', methods=['POST'])
 def delete(post_id):
     posts = load_posts()
@@ -58,6 +63,28 @@ def delete(post_id):
         return redirect(url_for('index'))
     else:
         abort(404)  # Handle the case where the post doesn't exist
+
+
+def fetch_post_by_id(post_id):
+    posts = load_posts()
+    return next((post for post in posts if post['id'] == post_id), None)
+
+
+@app.route('/update/<int:post_id>', methods=['GET', 'POST'])
+def update(post_id):
+    post = fetch_post_by_id(post_id)
+    if post is None:
+        abort(404)
+
+    if request.method == 'POST':
+        post['author'] = request.form.get('author')
+        post['title'] = request.form.get('title')
+        post['content'] = request.form.get('content')
+        save_posts(load_posts())  # Save the updated posts
+        return redirect(url_for('index'))
+
+    return render_template('update.html', post=post)
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
