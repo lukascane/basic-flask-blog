87ea0ed887d6cb21afcc8b2fbd8e8d592f76f9d4
LukasTrzcinski <lukastrz6@gmail.com>
2025-05-13 15:41:25 +0200

    add delate and update directory for html, add static for css

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, abort
 import json
 
 app = Flask(__name__)
-
 
 # Load posts from posts.json
 def load_posts():
@@ -12,37 +11,27 @@ def load_posts():
     except FileNotFoundError:
         return []  # Return an empty list if the file doesn't exist
 
-
 # Save posts to posts.json
 def save_posts(posts):
     with open('posts.json', 'w') as f:
         json.dump(posts, f, indent=4)
-
 
 @app.route('/')
 def index():
     posts = load_posts()
     return render_template('index.html', posts=posts)
 
-
 @app.route('/add', methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
-        # Get data from the form
         author = request.form.get('author')
         title = request.form.get('title')
         content = request.form.get('content')
 
-        # Load existing posts
         posts = load_posts()
 
-        # Generate a unique ID (Improved)
-        if posts:
-            next_id = max(post['id'] for post in posts) + 1
-        else:
-            next_id = 1
+        next_id = max(post['id'] for post in posts) + 1 if posts else 1
 
-        # Create the new post
         new_post = {
             'id': next_id,
             'author': author,
@@ -50,18 +39,25 @@ def add():
             'content': content
         }
 
-        # Add the new post to the list
         posts.append(new_post)
-
-        # Save the updated posts list
         save_posts(posts)
 
-        # Redirect to the index page
+        return redirect(url_for('index'))
+    return render_template('add.html')
+
+@app.route('/delete/<int:post_id>', methods=['POST'])
+def delete(post_id):
+    posts = load_posts()
+
+    # Find the post with the given ID
+    post_to_delete = next((post for post in posts if post['id'] == post_id), None)
+
+    if post_to_delete:
+        posts.remove(post_to_delete)
+        save_posts(posts)
         return redirect(url_for('index'))
     else:
-        # Render the form for adding a new post
-        return render_template('add.html')
-
+        abort(404)  # Handle the case where the post doesn't exist
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
